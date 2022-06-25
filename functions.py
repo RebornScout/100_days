303e87112630c6324887001cfb0c970551a1ec41
RebornScout <johnw@dobsons-home.me>
2022-06-25 21:58:21 +0100

    Added Day 10 - Calculator

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 # Various functions used across multiple 100 days projects.
 from os import system, name
+
 def clear():
+    """Check OS and then clear terminal screen"""
     # for windows
     if name == 'nt':
         _ = system('cls')

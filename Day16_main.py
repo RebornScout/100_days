eb553b953b44cb39f8f15c40973646b87d6d08c9
RebornScout <johnw@dobsons-home.me>
2022-07-25 21:41:09 +0100

    Added Day 16 OOP Coffee Machine

---
+++ Day16_main.py
@@ -0,0 +1,24 @@
+from Day16_menu import Menu, MenuItem
+from Day16_coffee_maker import CoffeeMaker
+from Day16_money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+switched_on = True
+
+while switched_on:
+    choice = input("What would you like? " + menu.get_items() + ": ")
+    if choice == "off":
+        switched_on = False
+        continue
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "latte" or choice == "espresso" or choice == "cappunccino":
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+    else:
+        continue
